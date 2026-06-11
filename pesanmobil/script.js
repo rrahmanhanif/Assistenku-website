6cf92a0a274ef64d4db3263bcb92f758ecb1933c
rrahmanhanif <rrahmanhanif@gmail.com>
2026-06-11 21:57:58 +0700

    Update script.js

--- pesanmobil/script.js
+++ pesanmobil/script.js
@@ -1,1 +1,12 @@
 console.log("Ride Booking Assistenku");
+const map = L.map('map').setView(
+[-6.2088,106.8456],
+10
+);
+
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+maxZoom:19
+}
+).addTo(map);
