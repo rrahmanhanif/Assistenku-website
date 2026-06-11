9ef91b7466a2dac47ad7851602efff6af751120a
rrahmanhanif <rrahmanhanif@gmail.com>
2026-06-11 22:22:04 +0700

    Update script.js

--- pesanmobil/script.js
+++ pesanmobil/script.js
@@ -1,98 +1,68 @@
 console.log("Ride Booking Assistenku Loaded");
 
-if(document.getElementById('map')){
+if (document.getElementById("map")) {
 
-const map = L.map('map').setView(
-[-6.2088,106.8456],
-10
-);
+  const map = L.map("map").setView(
+    [-6.2088, 106.8456],
+    10
+  );
 
-L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-{
-maxZoom:19
-}
-).addTo(map);
+  L.tileLayer(
+    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19
+    }
+  ).addTo(map);
 
-let pickupMarker = null;
-let destinationMarker = null;
+  let pickupMarker = null;
+  let destinationMarker = null;
 
-map.on('click', function(e){
+  map.on("click", function (e) {
 
-if(!pickupMarker){
+    if (!pickupMarker) {
 
-pickupMarker = L.marker(e.latlng)
-.addTo(map)
-.bindPopup("Titik Jemput")
-.openPopup();
+      pickupMarker = L.marker(e.latlng)
+        .addTo(map)
+        .bindPopup("Titik Jemput")
+        .openPopup();
 
-document.getElementById("pickup").innerText =
-e.latlng.lat.toFixed(6) +
-"," +
-e.latlng.lng.toFixed(6);
+      document.getElementById("pickup").innerText =
+        e.latlng.lat.toFixed(6) +
+        "," +
+        e.latlng.lng.toFixed(6);
 
-}
+    }
 
-else if(!destinationMarker){
+    else if (!destinationMarker) {
 
-destinationMarker = L.marker(e.latlng)
-.addTo(map)
-.bindPopup("Titik Tujuan")
-.openPopup();
+      destinationMarker = L.marker(e.latlng)
+        .addTo(map)
+        .bindPopup("Titik Tujuan")
+        .openPopup();
 
-document.getElementById("destination").innerText =
-e.latlng.lat.toFixed(6) +
-"," +
-e.latlng.lng.toFixed(6);
+      document.getElementById("destination").innerText =
+        e.latlng.lat.toFixed(6) +
+        "," +
+        e.latlng.lng.toFixed(6);
 
-}
+    }
 
-});
+    else {
 
-}
+      map.removeLayer(pickupMarker);
+      map.removeLayer(destinationMarker);
 
-map.on('click', function(e){
+      pickupMarker = null;
+      destinationMarker = null;
 
-if(!pickupMarker){
+      document.getElementById("pickup").innerText =
+        "Belum dipilih";
 
-pickupMarker = L.marker(e.latlng)
-.addTo(map)
-.bindPopup("Titik Jemput")
-.openPopup();
+      document.getElementById("destination").innerText =
+        "Belum dipilih";
 
-document.getElementById("pickup").innerText =
-e.latlng.lat.toFixed(6) + "," +
-e.latlng.lng.toFixed(6);
+    }
 
-}
-
-else if(!destinationMarker){
-
-destinationMarker = L.marker(e.latlng)
-.addTo(map)
-.bindPopup("Titik Tujuan")
-.openPopup();
-
-document.getElementById("destination").innerText =
-e.latlng.lat.toFixed(6) + "," +
-e.latlng.lng.toFixed(6);
+  });
 
 }
-
-else{
-
-map.removeLayer(pickupMarker);
-map.removeLayer(destinationMarker);
-
-pickupMarker = null;
-destinationMarker = null;
-
-document.getElementById("pickup").innerText =
-"Belum dipilih";
-
-document.getElementById("destination").innerText =
-"Belum dipilih";
-
-}
-
-});
